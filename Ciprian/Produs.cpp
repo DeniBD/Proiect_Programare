e496df9652222e11aba1cbbd7e1f5e8f1efd49e8
DanielBanci <93344852+DanielBanci@users.noreply.github.com>
2022-05-30 04:15:34 +0300

    Update Produs.cpp

--- Ciprian/Produs.cpp
+++ Ciprian/Produs.cpp
@@ -1,175 +1,68 @@
 //
-// Created by Daniel on 18-May-22.
+// Created by Daniel on 29-May-22.
 //
 
 #include "Produs.h"
-#include <cstring>
+#include <string>
 #include <iostream>
 
 using namespace std;
-///Produs
-
-Produs::Produs() {
-    this->nume = nullptr;
-    this->codBare = nullptr;
-    this->cantitate = 0;
-    this->pret = 0;
-    this->expData = nullptr;
+int Produs::next_id = 1;
+ostream &operator<<(ostream &os, Produs &x) {
+    os <<"["<<x.getID()<<"] \tNume produs: " << x.nume << endl << "\tPret produs: " << x.pret <<" lei"<< endl << "\tData expirare: " << *x.expData << endl;
+    return os;
 }
 
-Produs::Produs(char *n,char *cb,int cant,double pr,Data *d){
-    this->nume = new char[strlen(n)+1];
-    strcpy(this->nume,n);
-    this->codBare = new char[strlen(cb)+1];
-    strcpy(this->codBare,cb);
-    this->cantitate = cant;
-    this->pret = pr;
-    //this->expData = new Data;
-    this->expData = d;
+Produs::Produs(string nume, string codBare, int cantitate, double pret, Data *expData) {
+    id = next_id++;
+    this->nume = nume;
+    this->codBare = codBare;
+    this->cantitate = cantitate;
+    this->pret = pret;
+    if(expData != nullptr)
+        this->expData = expData;
 }
-
-Produs::Produs(const Produs &p) {
-    if(this->nume)
-        delete []this->nume;
-    this->nume = new char[strlen(p.nume)+1];
-    strcpy(this->nume,p.nume);
-    if(this->codBare)
-        delete []this->nume;
-    this->nume = new char[strlen(p.codBare)+1];
-    strcpy(this->codBare,p.codBare);
-    this->cantitate = p.cantitate;
-    this->pret = p.pret;
-    if(this->expData)
+Produs& Produs::operator=(const Produs & x) {
+    if(this->expData != nullptr)
         delete this->expData;
-    //this->expData = new Data;
-    this->expData = p.expData;
+    if(x.expData != nullptr) {
+        this->expData = new Data;
+        this->expData = x.expData;
+    }
+    this->pret = x.pret;
+    this->cantitate = x.cantitate;
+    this->nume = x.nume;
+    this->codBare = x.codBare;
+
+    return *this;
+}
+bool operator <(const Produs &x, const Produs &y) {
+
+}
+Produs::Produs() {
 }
 
 Produs::~Produs() {
-    if(this->nume)
-        delete []this->nume;
-    if(this->codBare)
-        delete []this->codBare;
-    if(this->expData)
-        delete []this->expData;
+    /*if(this->expData != nullptr)
+        delete this->expData;*/
 }
 
-void Produs::afisare() {
-    cout<<"Produs: ";
-    if(this->nume)
-        cout<<this->nume<<endl;
-    else
-        cout<<"Fara nume"<<endl;
-    cout<<"Cantitate: "<<this->cantitate<<endl;
-    cout<<"Pret: "<<this->pret<<endl;
-    cout<<"Data expirare: ";
-    if(this->expData)
-        this->expData->afisare();
-    cout<<endl;
+string Produs::get_nume() {
+    return nume;
 }
 
-void Produs::modif_cantitate(int c) {
-    this->cantitate += c;
-}
-///Data
-
-Data::Data() {
-    this->zi = 0;
-    this->luna = 0;
-    this->an = 0;
+double Produs::get_pret() {
+    return pret;
 }
 
-Data::Data(int z,int l,int a){
-    this->zi = z;
-    this->luna = l;
-    this->an = a;
+string Produs::get_cod_bare(){
+    return this->codBare;
 }
 
-Data::Data(const Data &d) {
-    this->zi = d.zi;
-    this->luna = d.luna;
-    this->an = d.an;
+int Produs::get_cantitate(){
+    return this->cantitate;
 }
 
-Data::Data(char *info) {
-    char *aux;
-    int z,l,a;
-    aux = strtok(info,".");
-    z = stoi(aux);
-    aux = strtok(NULL,".");
-    l = stoi(aux);
-    aux = strtok(NULL,".");
-    a = stoi(aux);
-    this->zi = z;
-    this->luna = l;
-    this->an = a;
-}
-
-void Data::afisare() {
-    cout<<this->zi<<"."<<this->luna<<"."<<this->an<<endl;
-}
-
-///Comanda
-
-Comanda::Comanda() {
-    this->produse = nullptr;
-    this->dataPlasare = nullptr;
-    this->pret = 0;
-    this->nrProduse = -1;
-}
-
-Comanda::Comanda(Produs **p, Data *d, double pr, int np) {
-    this->produse = new Produs*[np];
-    this->produse = p;
-    this->dataPlasare = new Data;
-    this->dataPlasare = d;
-    this->pret = pr;
-    this->nrProduse = np;
-}
-
-Comanda::Comanda(const Comanda &c) {
-    if(this->produse)
-        delete []this->produse;
-    this->produse = new Produs*[c.nrProduse];
-    this->produse = c.produse;
-    if(this->dataPlasare)
-        delete dataPlasare;
-    this->dataPlasare = new Data;
-    this->dataPlasare = c.dataPlasare;
-    this->nrProduse = c.nrProduse;
-}
-
-Comanda::~Comanda() {
-    if(this->produse)
-        delete []this->produse;
-    if(this->dataPlasare)
-        delete this->dataPlasare;
-}
-
-///CosCumparaturi
-
-CosCumparaturi::CosCumparaturi() {
-    this->produse = nullptr;
-    this->pret = 0;
-    this->nrProduse = -1;
-}
-
-CosCumparaturi::CosCumparaturi(Produs **p, double pr, int n) {
-    this->produse = new Produs*[n];
-    this->pret = pr;
-    this->nrProduse = n;
-}
-
-CosCumparaturi::CosCumparaturi(const CosCumparaturi &c) {
-    if(this->produse)
-        delete []this->produse;
-    this->produse = new Produs*[c.nrProduse];
-    this->produse = c.produse;
-    this->pret = c.pret;
-    this->nrProduse = c.nrProduse;
-}
-
-CosCumparaturi::~CosCumparaturi() {
-    if(this->produse)
-        delete []this->produse;
+Data* Produs::get_data(){
+    return this->expData;
 }
